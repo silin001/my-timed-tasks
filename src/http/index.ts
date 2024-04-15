ac3a034ee89c948254b45f370823fa4515c8553b
sisi <862956812@qq.com>
2024-04-15 10:31:16 +0800

    test: 时区修改测试

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,19 +1,18 @@
 /*
  * @Date: 2024-04-11 09:52:14
- * @LastEditTime: 2024-04-13 13:53:43
+ * @LastEditTime: 2024-04-15 09:17:53
  * @Description:
  * @FilePath: \yike-design-devd:\web_si\my_webDemo\my-projectFrame\my-timed-tasks\src\http\index.ts
  */
 
-// const http = require("http");
 import http from 'http'
 
 export const httpGet = (api:string) => {
   return new Promise((resolve, reject) => {
     http
       .get(api, (res) => {
-        let bufferData;
-        res.on("data", (chunk) => {
+        let bufferData : BufferSource;
+        res.on("data", (chunk: BufferSource) => {
           bufferData = chunk;
         });
         res.on("end", () => {
--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,6 +1,6 @@
 /*
  * @Date: 2024-04-12 17:20:56
- * @LastEditTime: 2024-04-13 13:20:48
+ * @LastEditTime: 2024-04-15 09:17:30
  * @Description:
  * @FilePath: \yike-design-devd:\web_si\my_webDemo\my-projectFrame\my-timed-tasks\src\utils\tools.ts
  */
@@ -9,9 +9,15 @@
 
 
 // 辅助函数，将样式对象转换为字符串
-export function getStyleString(styleObj: object) {
+export function getStyleString (styleObj: { [key : string]: string}) {
   return Object.keys(styleObj)
     .map(key => `${key}:${styleObj[key]}`)
     .join(';');
 }
+
+// export function getStyleString(styleObj: object) {
+//   return Object.keys(styleObj)
+//     .map(key => `${key}:${styleObj[key]}`)
+//     .join(';');
+// }
 
